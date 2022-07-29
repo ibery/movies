1d585e22591e6e9ee444d4be8441922bb7000e92
ibrahim eryılmaz <veteryilmaz@gmail.com>
2022-07-29 23:52:53 +0300

    Getting page control id completed

--- MobiMovie/Services/WebServices.swift
+++ MobiMovie/Services/WebServices.swift
@@ -16,14 +16,14 @@ class WebService {
     // MARK: - Properties
     var movies = [MovieModel]()
     var c = C()
-  //  var movie = MovieModel()
-
+    //  var movie = MovieModel()
+    
     
     // MARK: - Initialierz
     
     // With Alamofire
     
-  
+    
     
     
     // MARK: - Setup
@@ -31,36 +31,36 @@ class WebService {
     
     // MARK: - Actions
     
-
+    
     //MARK: - Methods
     
     func downloadMovieData (url : URL , completion: @escaping ([MovieModel]?) -> () ){
         AF.request(url, method: .get).validate().responseJSON{ response in
             switch response.result{
-                case.success(let value):
+            case.success(let value):
                 
-                 let json = JSON(value)
-               
-                                for index in 0...json.count{
-                                        let newMovies = MovieModel(title: json["results"][index]["title"].stringValue,
-                                                                   release_date: json["results"][index]["release_date"].stringValue,
-                                                               overview: json["results"][index]["overview"].stringValue,
-                                                               backdrop_path: json["results"][index]["backdrop_path"].stringValue,
-                                                               id: json["results"][index]["id"].intValue,
-                                                               vote_average: json["results"][index]["vote_average"].doubleValue)
-                                    self.movies.append(newMovies)
-                                    
-                                }
-                            completion(self.movies)
-                            case .failure(_):
-                                print("Error !")
-                            }
-                        }
+                let json = JSON(value)
+                
+                for index in 0...json.count{
+                    let newMovies = MovieModel(title: json["results"][index]["title"].stringValue,
+                                               release_date: json["results"][index]["release_date"].stringValue,
+                                               overview: json["results"][index]["overview"].stringValue,
+                                               backdrop_path: json["results"][index]["backdrop_path"].stringValue,
+                                               id: json["results"][index]["id"].intValue,
+                                               vote_average: json["results"][index]["vote_average"].doubleValue)
+                    self.movies.append(newMovies)
+                    
+                }
+                completion(self.movies)
+            case .failure(_):
+                print("Error !")
+            }
+        }
     }
-   
-
-
-}
     
+    
+    
+}
 
- 
+
+
--- MobiMovie/View/Main/MainViewController.swift
+++ MobiMovie/View/Main/MainViewController.swift
@@ -6,40 +6,41 @@ import SwiftyJSON
 
 class MainViewController :BaseViewController, UIScrollViewDelegate  {
     private var c = C()
-    private var movie : MovieModel?
+    private var movie : MovieModel!
     private var movieListModel : MovieListModel!
-   
-
-   
+    private var movieNowPlayingListModel : MovieListModel!
+    var moviesName : [String] = []
+    
     
     
     // MARK: - Properties
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var pageControl: UIPageControl!
     @IBOutlet var tableView: UITableView!
-    
- 
-    
-    var  movies : [ String ] = [ "American Beauty" , "Green Book" , "The Great Beauty" ,"There Will Be Blood"]
-     var  frame = CGRect .zero
+
+    var  frame = CGRect .zero
     
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let storyboard = UIStoryboard(name: "DetailScreen" , bundle: nil)
-//        guard let viewController = storyboard.instantiateInitialViewController() else { return }
-//        self.navigationController?.show(viewController, sender: nil)
-    
+        
+       
+        //        let storyboard = UIStoryboard(name: "DetailScreen" , bundle: nil)
+        //        guard let viewController = storyboard.instantiateInitialViewController() else { return }
+        //        self.navigationController?.show(viewController, sender: nil)
+        
+        getUpComingData()
+        getNowPlayingdata()
         tableView.delegate = self
         tableView.dataSource = self
         
         self.tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "mainCell")
         
-        pageControl.numberOfPages = movies.count
-        setupScreens()
+        pageControl.numberOfPages = moviesName.count
+        //  setupScreens()
         scrollView.delegate = self
-        getUpComingData()
+        
     }
     
     
@@ -48,38 +49,82 @@ class MainViewController :BaseViewController, UIScrollViewDelegate  {
         
     }
     
-  
+    
     
     // MARK: - Setup
-    
-    
-    // MARK: - Actions
-    
-
-    //MARK: - Methods
-    
     func setupScreens() {
-        for index in 0..<movies.count {
+        for index in 0..<moviesName.count {
             frame.origin.x = scrollView.frame.size.width * CGFloat(index)
             frame.size = scrollView.frame.size
             
-            let imgView = UIImageView(frame: frame)
-            imgView.image = UIImage(named: movies[index])
+            let  imgView = UIImageView(frame: frame)
+            imgView.isUserInteractionEnabled = true
+            
+            let recognizer = UITapGestureRecognizer(target: self, action: #selector(imageClick))
+            let movieViewModel = movieNowPlayingListModel.movieAtIndex(index)
+ 
+            let url = URL(string: self.c.imageUrl+movieViewModel.backdrop_path)
+            let data = try? Data(contentsOf: url!)
+            imgView.image =  UIImage(data: data!)
+            imgView.addGestureRecognizer(recognizer)
+            
+            
             
             self.scrollView.addSubview(imgView)
         }
-        
-        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(movies.count)), height: scrollView.frame.size.height)
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(moviesName.count)), height: scrollView.frame.size.height)
         scrollView.delegate = self
+    }
+    
+    // tap gesture recognizer
+    @objc func imageClick() {
+        print(movieNowPlayingListModel.movieAtIndex(Int(scrollView.contentOffset.x / scrollView.frame.size.width)).id)
+        
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
         pageControl.currentPage = Int(pageNumber)
+        
     }
     
     
+    // MARK: - Actions
     
+    
+    //MARK: - Methods
+    
+    
+    
+    // now playing movies
+    func getNowPlayingdata(){
+        
+        let url = URL(string: c.nowPlaying)!
+        WebService().downloadMovieData(url: url) { nowPlayingMovieList in
+            if let nowPlayingMovieList = nowPlayingMovieList {
+                self.movieNowPlayingListModel = MovieListModel (movieList : nowPlayingMovieList )
+                
+            }
+            
+            if let nowPlayingMovieList = nowPlayingMovieList {
+                for mv in nowPlayingMovieList{
+                    self.moviesName.append(mv.backdrop_path ?? "")
+                    
+                    
+                }
+                
+                DispatchQueue.main.async {
+                    self.setupScreens()
+                }
+
+            }
+            
+        }
+        
+    }
+    
+    
+    // up coming movies
     func getUpComingData(){
         
         let url = URL(string: c.upComing)!
@@ -87,7 +132,23 @@ class MainViewController :BaseViewController, UIScrollViewDelegate  {
         WebService().downloadMovieData(url: url) { movieList in
             if let movieList = movieList {
                 self.movieListModel = MovieListModel(movieList: movieList)
-                print(self.movieListModel)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    // details movie
+    
+    func getDetailData(url : String){
+        
+        let url = URL(string: url)!
+        
+        WebService().downloadMovieData(url: url) { movieList in
+            if let movieList = movieList {
+                self.movieListModel = MovieListModel(movieList: movieList)
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -100,10 +161,10 @@ class MainViewController :BaseViewController, UIScrollViewDelegate  {
 
 extension MainViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return self.movieListModel == nil ? 0 : self.movieListModel.numberOfRowInSection()
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "mainCell" , for:indexPath) as! MainTableViewCell
@@ -112,28 +173,33 @@ extension MainViewController : UITableViewDataSource {
         cell.filmTitle.text = movieViewModel.title
         cell.filmOverview.text = movieViewModel.overview
         cell.filmDate.text = movieViewModel.release_date
-        let url = URL(string: "https://image.tmdb.org/t/p/w1280/p8XnVA7zWZu7ZJsM1Cm9l7S9IH8.jpg")
-       
-        if let data = try? Data(contentsOf: url!) {
-              DispatchQueue.main.async {
-                  // Create Image and Update Image View
-                  cell.self.movieImage.image = UIImage(data: data)
-              }
-          }
-        
-   
-         
+        let url = URL(string: c.imageUrl+movieViewModel.backdrop_path)
+        let data = try? Data(contentsOf: url!)
+        cell.movieImage.image =  UIImage(data: data!)
         cell.arrowIcon.image = UIImage(named: "Arrow Icon")
-
+        
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let movieId = self.movieListModel.movieAtIndex(indexPath.row)
+        print("movie id \(movieId.id)")
+        
+        
+        
+        
 
+        
+        
+    }
+    
 }
 
 extension MainViewController : UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return tableView.frame.height/20
-//    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height/5
+    }
+    
+    
 }
 
